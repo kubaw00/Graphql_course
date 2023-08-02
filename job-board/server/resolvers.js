20a39a1a405dd6aac3af2b31dcf8b21ea7f6e20b
Jakub Widurek <jakubwidurek@gmail.com>
2023-08-02 17:10:20 +0200

    add custom errors to job and compamny query

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,11 +1,24 @@
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
+import { GraphQLError } from 'graphql';
 
 export const resolvers = {
   Query: {
     jobs: () => getJobs(),
-    job: (_root, { id }) => getJob(id),
-    company: (_root, { id }) => getCompany(id),
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw customError('No job found with id ' + id);
+      }
+      return job;
+    },
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw customError('No company find with id ' + id);
+      }
+      return company;
+    },
   },
 
   Company: {
@@ -20,4 +33,10 @@ export const resolvers = {
 
 function toISODate(value) {
   return value.slice(0, 'dd-mm-yyyy'.length);
+}
+
+function customError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  });
 }
